e81fb234aab703889e41f3d95da1f9d8f45b0194
º • 🌱 kai 🌱 • º <kaisprunger@gmail.com>
2025-05-11 22:44:16 -0400

    connecting db and hosting

--- src/app/api/server.ts
+++ src/app/api/server.ts
@@ -1,9 +1,7 @@
-const { Pool } = require('pg');
+const db = require('pg-promise')();
 
-export const pool = new Pool({
-    host: process.env.AUTH_DATABASE_HOST,
-    port: parseInt(process.env.AUTH_DATABASE_PORT || "5432", 10), // Default to 5432 if undefined
-    database: process.env.AUTH_DATABASE_NAME,
-    user: process.env.AUTH_DATABASE_USER,
-    password: process.env.AUTH_DATABASE_PASSWORD,
-}); 
+const dbConfig = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+const dbInstance = db(dbConfig);
